8533a29b6bc82c511b832744bc917df929552f10
Aspen721 <torin.list@gmail.com>
2016-05-06 04:06:28 -0500

    Comments For NFC Plus Minor Manifest Change - Final Changes From Torin
    I added comments to NFC code and disabled resizing the listview when
    opening the keyboard so the search doesn't look like shit upon closing
    it. Will find an actual fix for it over the summer.

--- app/src/main/java/activities/MainActivity.java
+++ app/src/main/java/activities/MainActivity.java
@@ -255,7 +255,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        // if nfc adapter is null, then it means that the device does not have an nfcAdapter.
+        // if nfc adapter is null, then it means that the device does not have an nfc reader.
         if(nfcAdapter != null && nfcAdapter.isEnabled())
         {
             Intent intent = new Intent(this, MainActivity.class).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
@@ -296,6 +296,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause()
     {
         super.onPause();
+        //disables nfc on pause if device supports nfc
         if(nfcAdapter != null && nfcAdapter.isEnabled())
             nfcAdapter.disableForegroundDispatch(this);
     }
@@ -303,48 +304,61 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
+        //if new intent is nfc
         if(intent.hasExtra(NfcAdapter.EXTRA_TAG))
         {
             Toast.makeText(this, "NfcIntent", Toast.LENGTH_SHORT).show();
+            //get ndef messages
             Parcelable[] parcelables = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            // read messages if there are any on tag
             if(parcelables != null && parcelables.length > 0)
                 readTextFromMessage((NdefMessage) parcelables[0]);
             else
                 Toast.makeText(this, "No NDEF messages found", Toast.LENGTH_SHORT).show();
         }
+        //else handle as a normal intent
         else
             setIntent(intent);
     }
 
+    //This function extracts urls from ndef messages and sends them to the browser activity
     private void readTextFromMessage (NdefMessage ndefMessage)
     {
         NdefRecord[] ndefRecords = ndefMessage.getRecords();
         if(ndefRecords != null && ndefRecords.length > 0)
         {
+            //first record has url
             NdefRecord ndefRecord = ndefRecords[0];
+            //get text from first record
             String tagContent = getTextFromNdefRecord(ndefRecord);
+            //create intent
             Intent intent = new Intent(this, BrowserActivity.class);
             intent.putExtra(URL, tagContent);
+            //send to browser activity
             startActivity(intent);
         }
         else
             Toast.makeText(this, "No NDEF records found", Toast.LENGTH_SHORT).show();
     }
 
+    //This function extracts the text from an ndef record
     public String getTextFromNdefRecord(NdefRecord ndefRecord)
     {
         String tagContent = null;
         try
         {
+            //decode ndef record based on ndef standards
             byte[] payload = ndefRecord.getPayload();
             String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
             int languageSize = payload[0] & 0063;
+            //store url in new string
             tagContent = new String(payload, languageSize + 1, payload.length - languageSize - 1, textEncoding);
         }
         catch (UnsupportedEncodingException e)
         {
             e.printStackTrace();
         }
+        //return url
         return tagContent;
     }
 
